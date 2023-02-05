076b71deb19d1b0764d6a437ad4446252ec9e077
tunixxx78 <73437970+tunixxx78@users.noreply.github.com>
2023-02-05 14:04:15 +0200

    final update?

--- GGJ2023/Assets/Scripts/Leaver.cs
+++ GGJ2023/Assets/Scripts/Leaver.cs
@@ -5,12 +5,23 @@ using UnityEngine;
 public class Leaver : MonoBehaviour
 {
     [SerializeField] GameObject objToRemove;
+    [SerializeField] Animator leverAnimator;
+
+    SfxManager sfx;
+
+    private void Awake()
+    {
+        leverAnimator = GetComponentInChildren<Animator>();
+        sfx = FindObjectOfType<SfxManager>();
+    }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Player"))
         {
             objToRemove.GetComponent<Rigidbody2D>().constraints = RigidbodyConstraints2D.None;
+            leverAnimator.SetTrigger("IsPulled");
+            sfx.buttoPress();
         }
     }
 }
